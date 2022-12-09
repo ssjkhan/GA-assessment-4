9c3b226c26424b4511094513dadd96ed9c2122c8
sk <jsckhan@outlook.com>
2022-12-09 14:33:37 -0500

    structure circles component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
+import Selector from "./components/selector/selector";
+import Circles from "./components/circles/circles";
 
 const App = () => {
 	return (
 		<div className="App">
 			<header className="App-header">UNIT 4 FINAL ASSESSMENT</header>
-			<main>YOUR TWO COMPONENTS WILL GO HERE</main>
+			<main>
+				<div>
+					<Selector></Selector>
+				</div>
+				<Circles></Circles>
+			</main>
 		</div>
 	);
 };
---
+++ src/components/circles/circles.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./circle.css";
+
+const Circles = () => {
+	return (
+		<div className="Circles">
+			<div className="selected">1</div>
+			<div>2</div>
+			<div>3</div>
+			<div>4</div>
+		</div>
+	);
+};
+
+export default Circles;
